756c8ce9538562ba2d6190e41f062d5fb93fd01d
SebC11 <seabass6027@gmail.com>
2021-01-07 17:52:56 -0500

    Lambda Finished. May want to rewrite parts but works for now

--- FunFactLambdas/src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
+++ FunFactLambdas/src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Date;
@@ -14,10 +13,8 @@ import java.util.Properties;
 
 import javax.mail.Address;
 import javax.mail.Message;
-import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
@@ -28,7 +25,6 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.*;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
@@ -39,51 +35,51 @@ public class LambdaFunctionHandler {
 	LambdaLogger logger;
 
 	public void handleRequest(ScheduledEvent event, Context context) {
+		//Logger for AWS Cloudwatch
 		logger = context.getLogger();
-		// TODO: Fetch Emails from Dynamo and throw them into a list
 		
-		
-		// Fetching Properties file
 		Properties prop = new Properties();
 		FileInputStream ip;
-		
+		//Reading config.properties file
+		//Change path if you modify location of config file
 		try {
-			ip = new FileInputStream("/FunFactLambdas/src/main/resources/config.properties");
+			ip = new FileInputStream("src/main/resources/config.properties");
 			prop.load(ip);
 			logger.log("Succesfully pulled Properties");
-		} catch (Exception e) {
+		} catch (Exception ex) {
 			logger.log("Failed to pull from config.properties");
+			logger.log("EXCEPTION:" + ex);
+			logger.log("REASON: " + ex.getMessage());
 			System.exit(1);
 		}
 		logger.log("Loading Properties");
-		// loading in properties
+		
 		try {
+			// loading in properties
 			String user = prop.getProperty("user");
-			String pw = prop.getProperty("password");
+			String pw = prop.getProperty("pw");
 			String subject = prop.getProperty("subject");
-			String smtpUrl = prop.getProperty("smtpUrl");
-			String smtpPort = prop.getProperty("smtpPort");
-			// Temp usage. Should probably upgrade to Cognito
 			String accessKeyId = prop.getProperty("accessKeyId");
 			String secretAccessKey = prop.getProperty("secretAccessKey");
-			// making sure config file is set up properly
-			if (user == null || pw == null || subject == null || smtpUrl == null || smtpPort == null) {
-				logger.log("Incorrect config file setup");
-				System.exit(1);
-			}
-			
+
+			//Dynamo Client to pull addresses
 			BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKeyId, secretAccessKey);
 			AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-					.withRegion("us-east-1")
+					.withRegion("us-east-2")
 					.withCredentials(new AWSStaticCredentialsProvider(awsCreds))
 					.build();
-
+			
+			//Pulling table
+			logger.log("Getting Emails from Dynamo");
 			ScanRequest scanRequest = new ScanRequest()
 			    .withTableName("fun_fact");
 			List<String> addressList = new ArrayList<String>();
 			ScanResult result = client.scan(scanRequest);
+			//Scan for all emails
+			//Have to convert the map to extract the emails from the objects
 			for (Map<String, AttributeValue> item : result.getItems()){
-			    addressList.add(item.get("email").toString());
+				String str = item.get("email").toString();
+			    addressList.add(str.substring(4,str.length()-2));
 			}
 			
 			//Now convert to address array
@@ -99,10 +95,17 @@ public class LambdaFunctionHandler {
 				}
 			}
 			
+			//getting facts from api
 			String funFact = fetchFact();
 			
-			// need to build the props
+			//Setting email sending properties
+			prop.put("mail.debug", "true");
+        	prop.put("mail.smtp.auth", "true");
+        	prop.put("mail.smtp.starttls.enable","true");
+        	
+			//build session with new properties
 			Session session = Session.getDefaultInstance(prop);
+			//Set up message properties using MimeMessage
 			MimeMessage msg = new MimeMessage(session);
 			try {
 				msg.setFrom(new InternetAddress(user));
@@ -117,21 +120,28 @@ public class LambdaFunctionHandler {
 				System.exit(1);
 			}
 			
+			//initiate sending procedure
 			Transport transport = session.getTransport("smtp");
 			try {
 				logger.log("Connecting to email server");    				
-				transport.connect(prop.getProperty("mail.smtp.host"), Integer.parseInt(prop.getProperty("mail.smtp.port")), user, pw);
+				transport.connect("smtp.gmail.com", 587, user, pw);
 				logger.log("Sending email message");
 				transport.sendMessage(msg, msg.getAllRecipients());
 				logger.log("Email sent");
 			} catch (Exception ex) {
+				logger.log("Error during sending phase");
 				logger.log("EXCEPTION:" + ex);
 				logger.log("REASON: " + ex.getMessage());
+			} finally {
+				//Have to close connection here in case error happens during send
+				transport.close();
 			}
+			
 		} catch (Exception ex) {
 			logger.log("Failed to send email");
 			logger.log("EXCEPTION:" + ex);
 			logger.log("REASON: " + ex.getMessage());
+			System.exit(1);
 		}
 
 	}
@@ -156,7 +166,7 @@ public class LambdaFunctionHandler {
 			}
 			in.close();
 			System.out.println(response.toString());
-			JSONObject obj = new JSONObject(response);
+			JSONObject obj = new JSONObject(response.toString());
 			return obj.getString("text");
 			
 		} catch (Exception ex) {
