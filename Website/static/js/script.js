ca6fd340f33b49577c7ef9ef29a11d34636a9fc4
SebC11 <seabass6027@gmail.com>
2021-01-11 20:40:59 -0500

    Front end connected to backend

---
+++ Website/static/js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+    console.log("Document Loaded");
+    AWS.config.region = 'us-east-2'; // Region
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: 'us-east-2:781da989-a4c3-4dd4-a716-da5e7090be7f',
+    });
+    var docClient = new AWS.DynamoDB.DocumentClient();
+    $('#subscribe').click(function(e) {
+        e.preventDefault();
+        console.log("Subscribing");
+        //Create item in Dynamo
+        let emailVal = $('#email').val();
+        let params = {
+            TableName: 'fun_fact',
+            Item: { 'email': emailVal }
+        };
+        docClient.put(params, function(err, data) {
+            if (err) {
+                alert("Failed to subscribe your email.");
+                console.log(err);
+            } else {
+                alert("Successfully subscribed for daily fun fact emails")
+            }
+        });
+
+
+    });
+
+    $('#unsubscribe').click(function(e) {
+        e.preventDefault();
+        console.log("Unubscribing");
+        //Create item in Dynamo
+        let emailVal = $('#email').val();
+        let params = {
+            email: emailVal
+        };
+        docClient.delete(params, function(err, data) {
+            if (err) {
+                alert("Failed to unsubscribe your email");
+            } else {
+                alert("Successfully unsubscribed from daily fun fact emails. :(")
+            }
+        });
+    });
+});
